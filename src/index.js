1ffe3e5dac0a406dff129746eb3df51e568afdd5
Teo Jin Cheng <jinchengproject@gmail.com>
2020-02-27 07:01:49 +0800

    use eslint locally

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT = 3000;
 
-const server = app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server started on port ${PORT}...`);
 });
